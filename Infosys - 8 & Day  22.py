96df2d22bb98924ee8431d633887f73161b2ec6c
padmashri23 <130970670+padmashri23@users.noreply.github.com>
2024-10-23 01:41:32 +0530

    23-10-2024

---
+++ Infosys - 8 & Day  22.py
@@ -0,0 +1,11 @@
+#program to implement heapsort by pushing all values onto a heap and then popping off the smallest values one at a time.
+import heapq as hq
+def heapsort(iterable):
+    h=[]
+    for value in iterable:
+        hq.heappush(h,value)
+    return[hq.heappop(h)for i in range(len(h))]
+print(heapsort([7,89,54,6,2,3,1,5,7,5,9,6,3,2,4,15,25,3,6,36,2,6,24]))
+
+#OUTPUT:
+[1, 2, 2, 2, 3, 3, 3, 4, 5, 5, 6, 6, 6, 6, 7, 7, 9, 15, 24, 25, 36, 54, 89]
