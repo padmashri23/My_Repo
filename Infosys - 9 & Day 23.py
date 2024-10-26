0ff1abd37e64a4f7527901288ea357d80cfaab95
padmashri23 <130970670+padmashri23@users.noreply.github.com>
2024-10-26 22:17:13 +0530

    26-10-2024

---
+++ Infosys - 9 & Day 23.py
@@ -0,0 +1,18 @@
+#Common Divisors
+
+import math
+class Solution:
+    def commDiv(self,a,b):
+        gcd_ab=math.gcd(a,b)
+        count=0
+        for i in range(1,int(math.sqrt(gcd_ab))+1):
+            if gcd_ab%i==0:
+                count+=1
+                if i!=gcd_ab//i:
+                    count+=1
+        return count
+
+  #OUTPUT:
+  12 24
+  #Your Output: 
+  6
