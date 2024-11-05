7ac3b9f084109b170c030f1d0d84234272ac285f
Padmashri S <130970670+padmashri23@users.noreply.github.com>
2024-11-05 09:59:35 +0530

    05-11-2024

---
+++ Infosys - 11 & Day 26.py
@@ -0,0 +1,24 @@
+#Check for Power
+import math
+class Solution:
+    def isPowerOfAnother(ob,X,Y):
+        if X==1:
+            return 1 if Y==1 else 0
+        power=math.log(Y)/math.log(X)
+        return 1 if power.is_integer() or abs(power-round(power))<1e-9 else 0
+
+
+#OUTPUT:
+Input:
+X = 2, Y = 8
+Output:
+1
+Explanation:
+23 is equal to 8.
+Input:
+X = 1, Y = 8
+Output:
+0
+Explanation:
+Any power of 1 is not 
+equal to 8.
