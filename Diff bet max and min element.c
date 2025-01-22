4532bf6c1d90bb48622f126bb48dd3d222213f70
Padmashri S <130970670+padmashri23@users.noreply.github.com>
2025-01-22 21:49:01 +0530

    Create Diff bet max and min element.c

---
+++ Diff bet max and min element.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#define MAX_SIZE 1000
+int main()
+{
+    int i,n,arr[MAX_SIZE],max,min,diff;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    max=arr[0];
+    min=arr[0];
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+        if(arr[i]<min)
+        {
+            min=arr[i];
+        }
+    }
+    diff=max-min;
+    printf("%d",diff);
+    return 0;
+}
